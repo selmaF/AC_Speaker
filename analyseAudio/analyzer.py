36b00645f8cf54147df68ec85959ebc5cf44cacd
MaikeF <maike_friedrich@posteo.de>
2020-06-21 01:22:39 +0200

    code geordnet

--- analyseAudio/analyzer.py
+++ analyseAudio/analyzer.py
@@ -1,37 +1,28 @@
 import parselmouth
 from parselmouth.praat import run_file
 import numpy as np
+import os
 from scipy.stats import ks_2samp
 from scipy.stats import ttest_ind
 import scipy.io.wavfile as wav
 import seaborn as sns
 import matplotlib.pyplot as plt
 import math
-
+import statistics
 
 class AudioAnalyzer:
 
-    def __init__(self, name, first_audio, audio_files_path):
+    def __init__(self, name, audio_files_path):
         self.name = name
-        self.audio_file = first_audio
+        self.audio_file = name + ".wav"
+        self.grid_file = name + ".TextGrid"
         self.audio_files_path = audio_files_path
         self.analyzed_values = {}
-        # for the userCaseAnalysis:
-        # userCaseValues["length_in_sec"]
-        # self.userCaseValues["pauses"]
-        # self.userCaseValues["rate_of_speech"] = z3
-        # self.userCaseValues["balance"]
-        # self.userCaseValues["mood"]
-
-    def analyzeWholeFile(self):
-        self.analyzeIntensity()
-        self.analyze()
-        self.printResults()
-        #self.testPlot()
+        self.analysis_done = False
 
     def setStandard(self):
         # write data in txt-file
-        if self.analyzed_values["pauses"] != 0:
+        if self.analysis_done:
             f = open('standard.txt', 'w')
             f.write(self.name + '\n')
             for key in self.analyzed_values.keys():
@@ -43,192 +34,172 @@ class AudioAnalyzer:
         return
 
     def getResults(self):
-        return self.analyzed_values
+        if self.analysis_done:
+            return self.analyzed_values
+        else:
+            print("Noch keine Analyseergebnisse vorhanden")
+            raise(ImportError)
+
 
     def printResults(self):
-        length = str(self.analyzed_values["length_in_sec"])
-        pauses = str(self.analyzed_values["pauses"])
-        print("\n")
-        print(self.name)
-        print("Bei einer Gesamtlänge von " + length + " Sekunden wurden " + pauses + " Pausen gemacht")
-        print("Die durchschnittlich gesprochenen Silben pro Sekunde betragen " + str(self.analyzed_values["rate_of_speech"]))
-        print("Das Verhältnis von geprochener Zeit zu der Gesamtzeit ist " + str(self.analyzed_values["balance"]))
-        print("Die semantische Analyse ergab:  " + self.analyzed_values["mood"])
+        if self.analysis_done:
+            length = str(self.analyzed_values["length_in_sec"])
+            pauses = str(self.analyzed_values["pauses"])
+            mean_pauses = self.analyzed_values["mean_of_pauses"]
+            print("\n")
+            print(self.name)
+            print("Die durchschnittliche Lautstärke beträgt %.2f dB" % self.analyzed_values["mean_intensity"])
+            print("Bei einer Gesamtlänge von " + length + " Sekunden wurden " + pauses + " Pausen mit einer durchschnittlichen Länge von %.2f sec gemacht" % mean_pauses)
+            print("Die durchschnittlich gesprochenen Silben pro Sekunde betragen " + str(self.analyzed_values["rate_of_speech"]))
+            print("Das Verhältnis von geprochener Zeit zu der Gesamtzeit ist " + str(self.analyzed_values["balance"]))
+            print("Die semantische Analyse ergab:  " + self.analyzed_values["mood"])
+        else:
+            print("noch keine Werte analysiert")
 
-    def testPlot(self):
-        # versuch zu plotten aus Parselmouth Documentation, Release 0.4.0.dev0
-        sns.set()  # Use seaborn's default style to make attractive graphsplt.rcParams['figure.dpi'] = 100# Show nicely large images in this noteboo
-        plt.rcParams['figure.dpi'] = 100  # Show nicely large images in this notebook
-        snd = parselmouth.Sound(self.audio_files_path + "/" + self.audio_file)
-        plotter = Plotter(snd)
-        plotter.draw_intensity()
-        #plotter.draw_spectrogram()
-
-    def runPraatFile(self):
+    def runMyspsolutionPraatFile(self):
         # added from myspsolution.py
-        try:
-            sound = self.audio_files_path + "/" + self.audio_file
-            sourcerun = "myspsolution.praat"
-            path = self.audio_files_path + "/"
-        except:
-            print("Try again the sound of the audio was not clear")
+        sound = self.audio_files_path + "/" + self.audio_file
+        sourcerun = "myspsolution.praat"
+        path = self.audio_files_path + "/"
+
         return run_file(sourcerun, -20, 2, 0.3, "yes", sound, path, 80, 400, 0.01, capture_output=True)
 
-    def analyzeIntensity(self):
+    def analyzeWavFile(self):
+        try:
+            object = self.runMyspsolutionPraatFile()
+            self.analyzed_values["length_in_sec"] = self.analyze_length()
+            self.analyzed_values["mean_intensity"] = self.analyze_intensity()
+            self.analyzed_values["pauses"] = self.analyze_num_pauses(object)
+            self.analyzed_values["mean_of_pauses"] = self.compute_pause_length(self.audio_files_path + "/" + self.grid_file)
+            self.analyzed_values["rate_of_speech"] = self.analyze_rate_of_speech(object)
+            self.analyzed_values["balance"] = self.analyze_balance(object)
+            self.analyzed_values["mood"] = self.analyze_mood(object)
+            self.analysis_done = True
+        except:
+            print("couldn't read file")
+
+    def analyze_length(self):
+        # length of the .wav file nach https://stackoverflow.com/questions/40651891/get-length-of-wav-file-in-samples-with-python
+        sound = self.audio_files_path + "/" + self.audio_file
+        if os.path.isfile(sound):
+            (source_rate, source_sig) = wav.read(sound)
+            duration_seconds = len(source_sig) / float(source_rate)
+            return math.ceil(duration_seconds)
+        else:
+            print("Couldn't find path to file")
+
+    def analyze_intensity(self):
+        # todo nicht allgemeine Lautstärke analysieren, sondern nur die in der gesprochenen Zeit
+        # nehme abschnitte sounding aus TextGrid und analysiere Lautstärke und mean
         snd = parselmouth.Sound(self.audio_files_path + "/" + self.audio_file)
         intensity = snd.to_intensity()
         intensity_points = intensity.values.T
-        mean = np.mean(intensity_points)
-        self.analyzed_values["mean_intensity"] = mean
+        spoken_intensity = intensity_points[intensity_points > 25]
+        mean = np.mean(spoken_intensity)
+        return mean
 
-    def analyze(self, section="whole"):
-
-        # added from myspsolution.py
-        sound = self.audio_files_path + "/" + self.audio_file
-
-        # length of the .wav file nach https://stackoverflow.com/questions/40651891/get-length-of-wav-file-in-samples-with-python
-        try:
-            (source_rate, source_sig) = wav.read(sound)
-            duration_seconds = len(source_sig) / float(source_rate)
-            # print("Länge des Audiofiles= ", duration_seconds)
-            self.analyzed_values["length_in_sec"] = math.ceil(duration_seconds)
-        except:
-            print("Try again the sound of the audio was not clear")
-
+    def analyze_num_pauses(self, parsel_object):
         # mysp.mysppaus(p,c)
-        try:
-            # count number of pauses
-            objects = self.runPraatFile()
-            # print(objects[0])  # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
-            z1 = str(objects[
-                         1])  # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
-            z2 = z1.strip().split()
-            z3 = int(z2[1])  # will be the integer number 10
-            # print("number_of_pauses=", z3)
-            self.analyzed_values["pauses"] = z3
-        except:
-            print("Try again the sound of the audio was not clear")
+        # count number of pauses
+        z1 = str(parsel_object[1])  # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2 = z1.strip().split()
+        z3 = int(z2[1])  # will be the integer number 10
+        return z3
 
+    def compute_pause_length(self, path_to_textGrid):
+        pauses_intervall = []
+        length_of_pause = []
+        with open(path_to_textGrid) as textGrid:
+            lines = textGrid.readlines()
+            for i, line in enumerate(lines):
+                words = line.split()
+                if "\"silent\"" in words:
+                    pauses_intervall.append(
+                        (lines[i - 2].split()[-1], lines[i - 1].split()[-1]))  # append beginning and end of the pause
+                    length_of_pause.append(
+                        float(lines[i - 1].split()[-1]) - float(lines[i - 2].split()[-1]))  # append length of the pause
+
+        mean_of_pauses = statistics.mean(length_of_pause)
+        return mean_of_pauses
+
+    def analyze_rate_of_speech(self, parsel_object):
         # mysp.myspsr(p, c)
-        try:
-            # print(objects[0])  # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
-            z1 = str(objects[1])  # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
-            z2 = z1.strip().split()
-            z3 = int(z2[2])  # will be the integer number 10
-            # print("rate_of_speech=", z3, "# syllables/sec original duration")
-            self.analyzed_values["rate_of_speech"] = z3
-        except:
-            print("Try again the sound of the audio was not clear")
 
+        z1 = str(parsel_object[1])  # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2 = z1.strip().split()
+        z3 = int(z2[2])  # will be the integer number 10
+        # print("rate_of_speech=", z3, "# syllables/sec original duration")
+        return z3
+
+    def analyze_balance(self, parsel_object):
         # mysp.myspbala(p,c)
-        try:
-            # print(objects[0])  # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
-            z1 = str(objects[
-                         1])  # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
-            z2 = z1.strip().split()
-            z4 = float(z2[6])  # will be the floating point number 8.3
-            # print("balance=", z4, "# ratio (speaking duration)/(original duration)")
-            self.analyzed_values["balance"] = z4
-        except:
-            print("Try again the sound of the audio was not clear")
 
+        z1 = str(parsel_object[1])  # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2 = z1.strip().split()
+        z4 = float(z2[6])  # will be the floating point number 8.3
+        return z4
+
+    def analyze_mood(self, parsel_object):
         # mysp.myspgend(p,c)
-        try:
-            # print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
-            z1 = str(objects[
-                         1])  # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
-            z2 = z1.strip().split()
-            z3 = float(z2[8])  # will be the integer number 10
-            z4 = float(z2[7])  # will be the floating point number 8.3
-            if z4 <= 114:
-                g = 101
-                j = 3.4
-            elif 114 < z4 <= 135:
-                g = 128
-                j = 4.35
-            elif 135 < z4 <= 163:
-                g = 142
-                j = 4.85
-            elif 163 < z4 <= 197:
-                g = 182
-                j = 2.7
-            elif 197 < z4 <= 226:
-                g = 213
-                j = 4.5
-            elif z4 > 226:
-                g = 239
-                j = 5.3
-            else:
-                print("Voice not recognized")
-                exit()
 
-            def teset(a, b, c, d):
-                d1 = np.random.wald(a, 1, 1000)
-                d2 = np.random.wald(b, 1, 1000)
-                d3 = ks_2samp(d1, d2)
-                c1 = np.random.normal(a, c, 1000)
-                c2 = np.random.normal(b, d, 1000)
-                c3 = ttest_ind(c1, c2)
-                y = ([d3[0], d3[1], abs(c3[0]), c3[1]])
-                return y
+        z1 = str(parsel_object[1])  # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2 = z1.strip().split()
+        z3 = float(z2[8])  # will be the integer number 10
+        z4 = float(z2[7])  # will be the floating point number 8.3
+        if z4 <= 114:
+            g = 101
+            j = 3.4
+        elif 114 < z4 <= 135:
+            g = 128
+            j = 4.35
+        elif 135 < z4 <= 163:
+            g = 142
+            j = 4.85
+        elif 163 < z4 <= 197:
+            g = 182
+            j = 2.7
+        elif 197 < z4 <= 226:
+            g = 213
+            j = 4.5
+        elif z4 > 226:
+            g = 239
+            j = 5.3
+        else:
+            print("Voice not recognized")
+            exit()
 
-            nn = 0
+        def teset(a, b, c, d):
+            d1 = np.random.wald(a, 1, 1000)
+            d2 = np.random.wald(b, 1, 1000)
+            d3 = ks_2samp(d1, d2)
+            c1 = np.random.normal(a, c, 1000)
+            c2 = np.random.normal(b, d, 1000)
+            c3 = ttest_ind(c1, c2)
+            y = ([d3[0], d3[1], abs(c3[0]), c3[1]])
+            return y
+
+        nn = 0
+        mm = teset(g, j, z4, z3)
+        while mm[3] > 0.05 and mm[0] > 0.04 or nn < 5:
             mm = teset(g, j, z4, z3)
-            while mm[3] > 0.05 and mm[0] > 0.04 or nn < 5:
-                mm = teset(g, j, z4, z3)
-                nn = nn + 1
-            nnn = nn
-            if mm[3] <= 0.09:
-                mmm = mm[3]
-            else:
-                mmm = 0.35
-            if 97 < z4 <= 114:
-                # print("a Male, mood of speech: Showing no emotion, normal")
-                self.analyzed_values["mood"] = "showing no emotion"
-            elif 114 < z4 <= 135:
-                # print("a Male, mood of speech: Reading")
-                self.analyzed_values["mood"] = "reading"
-            elif 135 < z4 <= 163:
-                # print("a Male, mood of speech: speaking passionately")
-                self.analyzed_values["mood"] = "speaking passionately"
-            elif 163 < z4 <= 197:
-                # print("a female, mood of speech: Showing no emotion, normal")
-                self.analyzed_values["mood"] = "showing no emotion"
-            elif 197 < z4 <= 226:
-                # print("a female, mood of speech: Reading")
-                self.analyzed_values["mood"] = "reading"
-            elif 226 < z4 <= 245:
-                # print("a female, mood of speech: speaking passionately")
-                self.analyzed_values["mood"] = "speaking passionately"
-            else:
-                print("Voice not recognized")
-                self.analyzed_values["mood"] = "Voice not recognized"
-        except:
-            print("Try again the sound of the audio was not clear")
-
-
-class Plotter:
-    # snd entspricht Ausgabe von parselmouth.Sound(path to wav-file)
-    def __init__(self, snd):
-        self.snd = snd
-        self.intensity = snd.to_intensity()
-        self.spectrogram = snd.to_spectrogram()
-
-    def draw_spectrogram(self, dynamic_range=70):
-        X, Y = self.spectrogram.x_grid(), self.spectrogram.y_grid()
-        sg_db = 10 * np.log10(self.spectrogram.values)
-        plt.pcolormesh(X, Y, sg_db, vmin=sg_db.max() - dynamic_range, cmap='afmhot')
-        plt.ylim([self.spectrogram.ymin, self.spectrogram.ymax])
-        plt.xlabel("time [s]")
-        plt.ylabel("frequency [Hz]")
-        plt.show()
-
-    def draw_intensity(self):
-        plt.figure()
-        plt.plot(self.intensity.xs(), self.intensity.values.T, linewidth=3, color='w')
-        plt.plot(self.intensity.xs(), self.intensity.values.T, linewidth=1)
-        plt.grid(False)
-        plt.ylim(0)
-        plt.ylabel("intensity [dB]")
-        plt.xlim([self.snd.xmin, self.snd.xmax])
-        plt.show()
+            nn = nn + 1
+        nnn = nn
+        if mm[3] <= 0.09:
+            mmm = mm[3]
+        else:
+            mmm = 0.35
+        if 97 < z4 <= 114:
+            return "showing no emotion"
+        elif 114 < z4 <= 135:
+            return "reading"
+        elif 135 < z4 <= 163:
+            return "speaking passionately"
+        elif 163 < z4 <= 197:
+            return "showing no emotion"
+        elif 197 < z4 <= 226:
+            return "reading"
+        elif 226 < z4 <= 245:
+            return "speaking passionately"
+        else:
+            print("Voice not recognized")
+            return "Voice not recognized"
--- analyseAudio/main.py
+++ analyseAudio/main.py
@@ -11,8 +11,8 @@ current_ratio = 0
 
 def find_frames(section_name, audio_frame, audio_files_path, data_whole_file):
 
-    analyzer_section = a.AudioAnalyzer(section_name, audio_frame, audio_files_path)
-    analyzer_section.analyze()
+    analyzer_section = a.AudioAnalyzer(audio_frame, audio_files_path)
+    analyzer_section.analyzeWavFile()
     data_section = analyzer_section.getResults()
 
     length_section = data_section["length_in_sec"]
@@ -20,6 +20,8 @@ def find_frames(section_name, audio_frame, audio_files_path, data_whole_file):
     rate_of_speech_section = data_section["rate_of_speech"]
     ratio_section = data_section["balance"]
     mood_section = data_section["mood"]
+    mean_intensity_section = data_section["mean_intensity"]
+    mean_pauses_section = data_section["mean_of_pauses"]
 
     # pauses/length
     pauses_length_section = (float(number_of_pauses_section)/float(length_section))
@@ -30,6 +32,10 @@ def find_frames(section_name, audio_frame, audio_files_path, data_whole_file):
         print("In " + section_name + " haben Sie mit " + str(number_of_pauses_section) + " Pausen mehr Pausen als im Durchschnitt gemacht")
     if pauses_length_section + 0.1 < pauses_length_whole:
         print("In " + section_name + " haben Sie mit " + str(number_of_pauses_section) + " Pausen weniger Pausen als im Durchschnitt gemacht")
+    if mean_intensity_section < (data_whole_file["mean_intensity"] - 4): # 10 ist random gewählt --> todo finde realistischen Schwellenwert
+        print("In " + section_name + " haben sie im Durchschnitt deutlich leiser gesprochen")
+    if mean_pauses_section > (data_whole_file["mean_of_pauses"] + 0.3 ):  # 0.3 ist random gewählt --> todo finde realistischen Schwellenwert
+        print("In " + section_name + " sind Ihre Pausen mit einer durchnittlichen Länge von %.2f deutlich länger als im Durchschnitt" % mean_pauses_section)
     if current_rate_of_speech > rate_of_speech_section:
         print("In " + section_name + " war Ihre Rede mit " + str(rate_of_speech_section) + " Silben pro Sekunde langsamer als im Durchscnitt")
     if current_rate_of_speech < rate_of_speech_section:
@@ -40,10 +46,9 @@ def find_frames(section_name, audio_frame, audio_files_path, data_whole_file):
     if mood_section != data_whole_file["mood"]:
         print("Die semantische Analyse von " + section_name + " unterscheidet sich mit \"" + mood_section + "\" von der der gesammten Rede")
 
-
 def split_audio_file(audio_file, last_second):
-    speech = AudioSegment.from_wav(audio_files_path + "/" + audio_file)
-    section_size = 20
+    speech = AudioSegment.from_wav(audio_files_path + "/" + audio_file + ".wav")
+    section_size = 15
     for number_section, section in enumerate(range(0, last_second, section_size)):
         section_speech = []
         section_start = section*1000
@@ -61,20 +66,21 @@ if __name__ == '__main__':
     try:
         first_audio = sys.argv[1]       # die wav Dateien sollten 44.100 Hz und 16 bit resolution besitzen
     except IndexError:
-        first_audio = "redeTed.wav"     # TODO: show error message (Daria)
-    audio_files_path = "data/myprosody/audioFiles"
+        first_audio = "redeTed"     # TODO: show error message (Daria)
+    audio_files_path = "data/audioFiles"
 
-    analyzer = a.AudioAnalyzer(first_audio, first_audio, audio_files_path)
-    analyzer.analyzeWholeFile()
-    data_whole = analyzer.getResults()
-   
+    analyzer = a.AudioAnalyzer(first_audio, audio_files_path)
+    analyzer.analyzeWavFile()
+    try:
+        analyzer.printResults()
+        data_whole = analyzer.getResults()
+    except ImportError:
+        print("Analyse noch nicht durchgeführt")
+
     #save_as_gold = input("\n Wollen Sie die Werte als Goldstandard speichern? Geben Sie ein j wenn ja " + "\n")
     #if save_as_gold == "j":
     #    analyzer.setStandard()
 
-    current_number_of_pauses = data_whole["pauses"]
-    current_rate_of_speech = data_whole["rate_of_speech"]
-    current_ratio = data_whole["balance"]
     number_of_sections = split_audio_file(first_audio, data_whole["length_in_sec"])
     for i in range(number_of_sections):
-        find_frames("Abschnitt " + str(i), "Abschnitt" + str(i) + ".wav", audio_files_path, data_whole)
+        find_frames("Abschnitt " + str(i), "Abschnitt" + str(i), audio_files_path, data_whole)
