e9430b537c11043dbe9d8e97d66c8833e1d28587
MaikeF <maike_friedrich@posteo.de>
2020-07-25 21:54:37 +0200

    gui in master integrieren

--- analyseAudio/analyzer.py
+++ analyseAudio/analyzer.py
@@ -2,10 +2,12 @@ import parselmouth
 from parselmouth.praat import run_file
 import numpy as np
 import os
-from scipy.stats import ks_2samp
-from scipy.stats import ttest_ind
 import scipy.io.wavfile as wav
 import math
+import sys
+import subprocess
+from pydub import AudioSegment
+
 import statistics
 import recognizer
 
@@ -40,6 +42,16 @@ class AudioAnalyzer:
         else:
             print("Noch keine Analyseergebnisse vorhanden")
             raise ImportError
+
+    def saveResults(self, path):
+        if self.analysis_done:
+            f = open(path + "/" + self.name + ".txt", 'w')
+            f.write(self.name + '\n')
+            for key in self.analyzed_values.keys():
+                f.write("%s, %s\n" % (key, self.analyzed_values[key]))
+        else:
+            print("noch keine Werte analysiert, daher gibt es keine Werte zu speichern")
+        return
 
     def printResults(self):
         if self.analysis_done:
@@ -178,48 +190,7 @@ class AudioAnalyzer:
         z2 = z1.strip().split()
         z3 = float(z2[8])  # will be the integer number 10
         z4 = float(z2[7])  # will be the floating point number 8.3
-        if z4 <= 114:
-            g = 101
-            j = 3.4
-        elif 114 < z4 <= 135:
-            g = 128
-            j = 4.35
-        elif 135 < z4 <= 163:
-            g = 142
-            j = 4.85
-        elif 163 < z4 <= 197:
-            g = 182
-            j = 2.7
-        elif 197 < z4 <= 226:
-            g = 213
-            j = 4.5
-        elif z4 > 226:
-            g = 239
-            j = 5.3
-        else:
-            print("Voice not recognized")
-            exit()
 
-        def teset(a, b, c, d):
-            d1 = np.random.wald(a, 1, 1000)
-            d2 = np.random.wald(b, 1, 1000)
-            d3 = ks_2samp(d1, d2)
-            c1 = np.random.normal(a, c, 1000)
-            c2 = np.random.normal(b, d, 1000)
-            c3 = ttest_ind(c1, c2)
-            y = ([d3[0], d3[1], abs(c3[0]), c3[1]])
-            return y
-
-        nn = 0
-        mm = teset(g, j, z4, z3)
-        while mm[3] > 0.05 and mm[0] > 0.04 or nn < 5:
-            mm = teset(g, j, z4, z3)
-            nn = nn + 1
-        nnn = nn
-        if mm[3] <= 0.09:
-            mmm = mm[3]
-        else:
-            mmm = 0.35
         if 97 < z4 <= 114:
             return "showing no emotion"
         elif 114 < z4 <= 135:
@@ -236,3 +207,76 @@ class AudioAnalyzer:
             print("Voice not recognized")
             return "Voice not recognized"
 
+def split_and_save_audio_file(audio_file, audio_files_path, section_folder, last_second):
+    speech = AudioSegment.from_wav(audio_files_path + "/" + audio_file + ".wav")
+    section_size = 15 # todo wo kann man diesen Parameter einstellen? In der GUI?
+    for number_section, section in enumerate(range(0, last_second, section_size)):
+        section_speech = []
+        section_start = section*1000
+        section_end = (section+section_size)*1000-1
+        section_speech = speech[section_start:section_end]
+        section_speech.export((section_folder + "/" + audio_file + "_section" + str(number_section+1) + ".wav"),
+                              format="wav", bitrate="44.1k")
+    return number_section+1
+
+def extract_audio_file(video_file_path, video_file_name):
+    # read video file, create audio file stub
+    video_file = video_file_path + "/"+ video_file_name + ".mp4"
+    first_audio = "{}_audio".format(video_file)
+
+    # extract audio track and save it with the marker "_audio"
+    command = "ffmpeg -i " + video_file + " -ab 160k -ac 2 -ar 44100 -vn {}.wav".format(first_audio)
+    subprocess.call(command, shell=True)
+    return str(video_file + "_audio.wav")
+
+def analyze_whole_and_sections(audio_file_name, audio_files_path):
+
+    #audio_file_name = "SusanDavid_2017W_kurz"  # TODO: show error message (Daria)
+    #audio_files_path = "data/audioFiles"
+    results_path = "data/results"
+
+    # analyze whole File
+    analyzer = AudioAnalyzer(audio_file_name, audio_files_path)
+    analyzer.analyzeWavFile()
+
+    try:
+        # analyzer.printResults()
+        analyzer.saveResults(results_path)
+        data_whole = analyzer.getResults()
+    except ImportError:
+        print("Analyse noch nicht durchgeführt")
+
+    # save_as_gold = input("\n Wollen Sie die Werte als Goldstandard speichern? Geben Sie ein j wenn ja " + "\n")
+    # if save_as_gold == "j":
+    #    analyzer.setStandard("data")
+
+    # split sections and save to data/audioFiles/sections:
+    # 1. delete old sections
+    section_folder = audio_files_path + "/sections"
+    try:
+        os.mkdir(section_folder, mode=0o777)
+    except FileExistsError:
+        print("Old sections will be deleted")
+        delete_folder_content(section_folder)
+    # 2. save new sections
+    number_of_sections = split_and_save_audio_file(audio_file_name, audio_files_path, section_folder,
+                                                   data_whole["length_in_sec"])
+    sections_results = []
+    for i in range(number_of_sections):
+        analyzer_section = AudioAnalyzer(audio_file_name + "_section" + str(i + 1), section_folder)
+        analyzer_section.analyzeWavFile()
+        sections_results.append(analyzer_section.getResults())
+        # find_frames(audio_file_name + "_section" + str(i+1), section_folder, data_whole)
+    return data_whole, sections_results
+
+def delete_folder_content(path_to_folder):
+
+    for filename in os.listdir(path_to_folder):
+        file_path = os.path.join(path_to_folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
---
+++ analyseAudio/main_window.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import statistics_window
+import sys
+
+class Ui_MainWindow(object):
+    def open_statistic_window(self):
+        self.window = QtWidgets.QMainWindow()
+        self.ui = statistics_window.Ui_statistics_window()
+        self.ui.setupUi(self.window)
+        self.window.show()
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(702, 369)
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(90, 40, 481, 101))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+
+        self.savedButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.savedButton.setObjectName("savedButton")
+        self.verticalLayout.addWidget(self.savedButton)
+        self.savedButton.clicked.connect(self.open_statistic_window)
+
+        self.recordButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.recordButton.setObjectName("recordButton")
+        self.verticalLayout.addWidget(self.recordButton)
+
+        self.compareButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.compareButton.setObjectName("compareButton")
+        self.verticalLayout.addWidget(self.compareButton)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 702, 22))
+        self.menubar.setObjectName("menubar")
+
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.savedButton.setText(_translate("MainWindow", "Analysiere gespeichertes Video"))
+        self.recordButton.setText(_translate("MainWindow", "Nehme Video zum Analysieren auf"))
+        self.compareButton.setText(_translate("MainWindow", "Vergleiche ältere Ergebnisse"))
+
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
